02ca6db509ea993345bafe48ff56b63606a0feff
Akhand Awasthi <2004akhand@gmail.com>
2025-06-11 22:16:40 +0530

    fhfhfh

--- src/app/api/users/forgotpassword/route.ts
+++ src/app/api/users/forgotpassword/route.ts
@@ -20,6 +20,8 @@ export async function POST(request: NextRequest){
         });
         if (!user) {
             return NextResponse.json({error:"Invalid or expired token"}, {status:401});
+
+            
         }
 
 
